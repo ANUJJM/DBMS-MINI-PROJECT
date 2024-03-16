993a3f8c530ed01efad565f52839f80811c91670
ANUJ J M <anujjm10@gmail.com>
2024-03-16 20:13:30 +0530

    Update urls.py

--- watersupply/supply/urls.py
+++ watersupply/supply/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
-    path('cart', views.cart, name='cart'),
+    # path('cart', views.cart, name='cart'),
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
     path('company/<int:company_id>/', views.company_products, name='company_products'),
@@ -15,7 +15,13 @@ urlpatterns = [
     path('myaccount', views.myaccount, name='myaccount'),
     path('checkout/', views.checkout, name='checkout'),
     path('myorder', views.myorder, name='myorder'),
-    
+    # added
+    path('product-list/', product_list, name='product-list'),
+    path('add-to-cart/<int:product_id>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<int:product_id>/', remove_from_cart, name='remove-from-cart'),
+    path('cart/', view_cart, name='cart'),
+    path('increase-cart-item/<int:product_id>/', increase_cart_item, name='increase-cart-item'),
+    path('decrease-cart-item/<int:product_id>/', decrease_cart_item, name='decrease-cart-item'),    
       # Ensure the name is 'company_products'
     # Example pattern
     # Add more URL patterns here as needed
