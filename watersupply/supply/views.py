c90f382ff64738c3e97b259987a3a60d181ad632
ANUJ J M <anujjm10@gmail.com>
2024-03-16 20:02:32 +0530

    Update views.py

--- watersupply/supply/views.py
+++ watersupply/supply/views.py
@@ -3,6 +3,10 @@ from urllib import request
 from django.shortcuts import redirect, render
 from .models import Company, Bottle
 from django.contrib.auth import authenticate, login as auth_login
+from .models import Product, Cart, CartItem
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def home(request):
     companies = Company.objects.all()
@@ -64,3 +68,60 @@ def login_view(request):
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'product_list.html', {'products': products})
+
+@login_required(login_url='login')
+def add_to_cart(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart_item, item_created = CartItem.objects.get_or_create(cart=cart, product=product)
+    
+    if not item_created:
+        cart_item.quantity += 1
+        cart_item.save()
+    
+    return redirect('product-list')
+
+@login_required(login_url='login')
+def remove_from_cart(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    cart = Cart.objects.get(user=request.user)
+    try:
+        cart_item = cart.cartitem_set.get(product=product)
+        if cart_item.quantity >= 1:
+             cart_item.delete()
+    except CartItem.DoesNotExist:
+        pass
+    
+    return redirect('cart')
+
+
+@login_required(login_url='login')
+def view_cart(request):
+    cart = request.user.cart
+    cart_items = CartItem.objects.filter(cart=cart)
+    return render(request, 'cart.html', {'cart_items': cart_items})
+
+@login_required(login_url='login')
+def increase_cart_item(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    cart = request.user.cart
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+
+    cart_item.quantity += 1
+    cart_item.save()
+
+    return redirect('cart')
+
+@login_required(login_url='login')
+def decrease_cart_item(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    cart = request.user.cart
+    cart_item = cart.cartitem_set.get(product=product)
+
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+
+    return redirect('cart')
